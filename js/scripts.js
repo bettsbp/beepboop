764f55f3c0c4e8600b74c532056e7d3289eb6e5a
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-08 14:57:54 -0700

    add beginnings to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,35 @@
 
 function toBeepBoop(number) {
   var numbers = number.toString().split('')
-  var firstArray = []
-  var finalNumber = ''
-  
+  var modifiedArray = []
+  var resultArray = []
+  var result = resultArray.toString()
 
+
+  for (i=0; i<numbers.length; i++) {
+    if (numbers[i].toString().includes('0')) {
+      resultArray.push('Beep')
+    } else if (numbers[i].toString().includes('1')) {
+      resultArray.push('Boop')
+    } else if (numbers[i] / 3 === 0) {
+      resultArray.push("I'm sorry Dave, I can't do that")
+    } else {
+      results.push(numbers[i])
+    }
+  }
 }
-
 
 // user logic
 
-
 $(document).ready(function() {
   $('#ogNumber').submit(function(event) {
-    alert('working');
+
     event.preventDefault();
+
+    var number = $('input#ogNumber').val();
+
+    toBeepBoop(number);
+
+    // alert(resultArray);
   })
 })
-
-// CRYPTOSQUARE
