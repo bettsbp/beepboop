a2a3ad7f9f64a48a772357a52e824eee6d60ed58
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-08 14:07:58 -0700

    add beginnings to content to index and js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// business logic
+
+function toBeepBoop(number) {
+
+var hasZero = number.contains('0')
+var hasOne = number.contains('1')
+
+// user logic
+
+
+$(document).ready(function() {
+  $('#ogNumber').submit(function(event) {
+    alert('working');
+    event.preventDefault();
+
+  })
+})
+
+// CRYPTOSQUARE
