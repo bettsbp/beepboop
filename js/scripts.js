ef20899d90538e6edd1d10140835910220891176
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-08 15:04:42 -0700

    add more code, not functional, but im saving here

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 // business logic
 
 function toBeepBoop(number) {
-  var numbers = number.toString().split('')
-  var modifiedArray = []
   var resultArray = []
-  var result = resultArray.toString()
 
 
-  for (i=0; i<numbers.length; i++) {
-    if (numbers[i].toString().includes('0')) {
+
+  for (i=0; i<number.length; i++) {
+    if (number[i].toString().includes('0')) {
       resultArray.push('Beep')
-    } else if (numbers[i].toString().includes('1')) {
+    } else if (number[i].toString().includes('1')) {
       resultArray.push('Boop')
-    } else if (numbers[i] / 3 === 0) {
-      resultArray.push("I'm sorry Dave, I can't do that")
+    } else if (number[i] / 3 === 0) {
+      resultArray.push("I'm sorry Dave, I can't do that") // WHY IS THIS NOT WORKING
     } else {
-      results.push(numbers[i])
+      resultArray.push(number[i])
+      alert(resultArray)
     }
   }
 }
