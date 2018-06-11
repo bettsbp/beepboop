5fa3ed459b3d14a44146fa504b496bc49acf7d90
Guest User <Guest@Epicodus-8A.local>
2018-06-11 14:48:28 -0700

    fix issue with 0 turning into dave message, remove comments

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function toBeepBoop(number) {
 
   var resultArray = []
 
-  for (index=0; index<=number; index++) { // dont set this to length, it explodes
-    if (index % 3 === 0) {
+  for (index=0; index<=number; index++) {
+    if (index % 3 === 0 && index != 0) {
       resultArray.push(' ' + "I'm sorry Dave, I can't do that")
     } else if (index.toString().includes('1')) {
       resultArray.push(' ' + 'Boop')
-    } else if (index.toString().includes('0')) { // this also just needs to be i and not the position in the array
-      resultArray.push(' ' + 'Beep') // WHY IS THIS NOT WORKING
+    } else if (index.toString().includes('0')) {
+      resultArray.push(' ' + 'Beep')
     } else {
       resultArray.push(' ' + index)
     }
@@ -30,5 +30,5 @@ $(document).ready(function() {
     $('#results').show();
     $('ul').append('<li><em>' + toBeepBoop(number) + '</em></li>');
 
-  })
-})
+  });
+});
